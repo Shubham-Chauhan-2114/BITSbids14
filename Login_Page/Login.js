83cc505e5ce27089c5bb97ef0e2264ab22b98ceb
Shubham-Chauhan-2114 <shubhamchauhancode@gmail.com>
2023-09-04 22:29:59 +0530

    login styles added

---
+++ Login_Page/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Login.css'; // Import your CSS file
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // Add your login logic here
+    if (username === 'your_username' && password === 'your_password') {
+      alert('Login successful!');
+      // Redirect to the dashboard or another page
+      // Example: history.push('/dashboard');
+    } else {
+      alert('Login failed. Please check your credentials.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
